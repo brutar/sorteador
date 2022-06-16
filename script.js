36e6f0dacc729c4f2d9a88370c68e48444ed4332
brutar <bruno.tar.gz@gmail.com>
2022-06-16 11:41:39 -0300

    Primeiro commit arquivos principais

---
+++ script.js
@@ -0,0 +1,28 @@
+const btnSortear = document.querySelector('#btnS');
+
+btnSortear.addEventListener("click", function(e){
+    e.preventDefault();
+    document.querySelector('#numSorteado').innerHTML = "";
+    //escolhe um numero randomicamente
+    function sorteaNumero(min,max){
+             return Math.floor(Math.random()*(max - min +1)  + min);
+    }
+    //limpa os campos de entrada
+    function limpaCampo(){
+        document.getElementById('campo1').value = '';
+        document.getElementById('campo2').value = '';
+    }
+    //pega os numeros dos campos de entrada
+    var nI = parseInt(document.getElementById('campo1').value);
+    var nF = parseInt(document.getElementById('campo2').value);
+    //valida se é numero inteiro maior ou igual a 0
+    if ((typeof nI == 'number'&&  !isNaN(nI) && nI >= 0 ) && (typeof nF == 'number' && !isNaN(nF) && nF >=0)){
+        var numR = sorteaNumero(nI,nF);
+        //mostra numero sorteado
+        setTimeout(()=>{document.querySelector('#numSorteado').innerHTML = parseInt(numR)},1000);
+    }else{
+      limpaCampo();
+      //mostra msg caso a validação não passe
+      document.querySelector('#numSorteado').innerHTML = "Digite um numero inteiro maior ou igual a 0";
+    }
+});
